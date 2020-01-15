45894d9dd01e53c1d3a018360a082e1ba4abd50c
wyy <2321287195@qq.com>
2020-01-15 19:44:18 +0800

    列表文章加载

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -4,7 +4,30 @@
     <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="refreshSuccessText">
       <!-- 上拉加载 -->
       <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-        <van-cell :title="item" v-for="item in articles" :key="item" />
+        <van-cell v-for="item in articles" :key="item">
+          <div class="article_item">
+            <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+            <!-- 三图模式 -->
+            <div class="img_box">
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </div>
+            <!-- 单图模式 -->
+            <div class="img_box">
+              <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </div>
+            <!-- 底部评论 -->
+            <div class="info_box">
+              <span>你像一阵风</span>
+              <span>8评论</span>
+              <span>10分钟前</span>
+              <span class="close">
+                <van-icon name="cross"></van-icon>
+              </span>
+            </div>
+          </div>
+        </van-cell>
       </van-list>
     </van-pull-refresh>
   </div>
@@ -18,7 +41,7 @@ export default {
       finished: false, // 完成
       articles: [], // 存放的数组
       downLoading: false, // 是否开启----下拉刷新---状态
-      refreshSuccessText: '更新成功'// 下拉显示的文本
+      refreshSuccessText: '更新成功' // 下拉显示的文本
     }
   },
   methods: {
@@ -41,7 +64,8 @@ export default {
     // 下拉刷新--数据添加在头部
     onRefresh () {
       // console.log('下拉刷新')
-      setTimeout(() => { // setTimeout模拟延迟
+      setTimeout(() => {
+        // setTimeout模拟延迟
         let arr = Array.from(Array(10), (value, index) => '追加' + index + 1) // 快速生成的假数据方法(模拟数据,索引从0开始，加1为了直观看效果)
         this.articles.unshift(...arr) // 将新数据添加在数组头部
         this.downLoading = false
@@ -53,4 +77,44 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
